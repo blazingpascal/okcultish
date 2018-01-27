2f0f8ffd028dc43367ac7540665176299f03c879
Michael Wong <emporophobe@gmail.com>
2018-01-27 00:48:10 -0500

    compliment generator

--- Assets/Scripts/Util/TestRunner.cs
+++ Assets/Scripts/Util/TestRunner.cs
@@ -4,11 +4,15 @@ using AssemblyCSharp;
 
 public class TestRunner : MonoBehaviour
 {
-
+	private System.Random random = new System.Random();
 	// Use this for initialization
 	void Start ()
 	{
 		User user1 = User.userGenerator ();
+		ComplimentGenerator cg = new ComplimentGenerator ();
+		for (int i = 0; i < 10; i++) {
+			print (cg.generateCompliment (random));
+		}
 		print ("HELLO");
 	}
 	

291bb6a28da9086a5ab44a706b833e03eced1eff
Michael Wong <emporophobe@gmail.com>
2018-01-28 12:21:32 -0500

    basic music playing

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+
+    private static MusicManager instance = null;
+    public static MusicManager Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
